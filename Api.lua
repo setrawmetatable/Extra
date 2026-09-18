c3ef31c2826ed10251d753bee20f7efef8e9b030
setrawmetatable <scarary1337@gmail.com>
2026-09-18 22:39:55 +0800

    Update Api.lua

--- Api.lua
+++ Api.lua
@@ -91,6 +91,7 @@ end
 
 function Api:LoadEsp()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/setrawmetatable/Extra/refs/heads/main/Esp.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/setrawmetatable/Extra/refs/heads/main/Visual"))()
     if getgenv().Esp then
         Api.Esp = getgenv().Esp
     end
